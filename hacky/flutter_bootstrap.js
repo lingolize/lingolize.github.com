e9e5e32dd53ea4a06e6d06220c90bffd0b7ea963
Lingolize hackyStories <stories@lingolize.com>
2024-12-21 17:12:41 +0000

    de4b6046cd1fb6c76723ae60adfeed97ec907904

--- hacky/flutter_bootstrap.js
+++ hacky/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3894309683"
+    serviceWorkerVersion: "2549077323"
   }
 });
